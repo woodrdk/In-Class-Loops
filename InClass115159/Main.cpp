f05e0bbb6b0bf9b92310fb8d7c3195db1b0ddec4
Robert Wood Jr <rdrwood@gmail.com>
2019-01-15 13:45:20 -0800

    Setup Inclass 11519

---
+++ InClass115159/Main.cpp
@@ -0,0 +1,20 @@
+
+// Robert M Wood Jr.
+// CPW 218
+// 01/15/19
+// In-Class Loops
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a = 2;
+
+	if (a = 3) cout << "a is 3" << endl;
+
+	system("pause");	// press key to continue
+	return 0;
+}
